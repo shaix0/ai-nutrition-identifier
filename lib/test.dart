a2a390ae12557cf1b10b6d4a0d03d66b04897183
ga Naaa <mix9958@gmail.com>
2025-11-03 01:00:54 +0800

    1102

---
+++ lib/test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+
+// Ideal time to initialize
+await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+//...
+}
